97efdd57650ebacbc8b94d8d8f230f401678010b
Helena <helenabf1@al.insper.edu.br>
2026-04-27 11:03:45 -0300

    primeiro

---
+++ funcoes.py
@@ -0,0 +1,10 @@
+import random
+def rolar_dados (n):
+    nova = []
+    i = 0
+    while i<=n:
+        valor = 0
+        valor = random.randint(1,6)
+        nova.append(valor)
+        i += 1
+    return nova
