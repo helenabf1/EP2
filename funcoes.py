2a8857b8a94977808aba2a65fa546c9e9464ccc4
Helena <helenabf1@al.insper.edu.br>
2026-04-28 12:42:37 -0300

    ex 4

--- funcoes.py
+++ funcoes.py
@@ -25,4 +25,11 @@ def remover_dado (rolados, estoque, n):
     nova.append(estoque)
     return nova
 
+def calcula_pontos_regra_simples (lista):
+    novo = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
+    for num in lista:
+        novo[num] += num
+    return novo
+        
+
 
